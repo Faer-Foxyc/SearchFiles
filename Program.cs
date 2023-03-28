4f1bd6f07e0922a0b0189706385947d5d09d4b2b
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-03-28 21:57:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter dirrectory for search: "); // Добавить бесконечный цикл для ввода директории, как только ввел выходим
+            string _MyDirecTory = Console.ReadLine();
+            if (_MyDirecTory == string.Empty)
+            {
+                Console.WriteLine("Enter dirrectory for search:  ");
+                _MyDirecTory= Console.ReadLine();
+            }
+
+            Console.Write("Enter extension for search: ");
+            string _Extension = Console.ReadLine();
+            if (_Extension == string.Empty)
+            {
+                Console.Write("Enter extension for search: ");
+                _Extension = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            _FileInfo _File = new _FileInfo();
+            _File.GetFiles(_MyDirecTory, _Extension);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _FileInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp3
+{
+    internal class _FileInfo
+    {
+        public string _Extension { get; set; }
+        public string _FileName { get; set; } = string.Empty;   
+        public long _FileSize { get; set; }
+
+        public void GetFiles(string _Directory, string _Extension)
+        {
+            DirectoryInfo directory= new DirectoryInfo(_Directory);
+
+            if (directory.Exists)
+            {
+                // Делаем обход папки
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    
+                    _FileName = file.Name;      // Получаем название файла
+                    _FileSize = file.Length;
+
+                    // Проверям, есть ли подстрока в названии файла
+                    if (file.Extension == _Extension)
+                    {
+                        Console.WriteLine("File Name: {0}, File Size: {1}", _FileName, _FileSize);
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Directory is not found");
+            }
+
+        }
+
+    }
+}
