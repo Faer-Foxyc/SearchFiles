9d86a5f9001e6f8738d73d6d1b43b272788be933
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-03-29 07:58:07 +0300

    Удаление неиспользуемых пространств имен

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp3
+
+namespace SearchFiles
 {
     internal class Program
     {
--- _FileInfo.cs
+++ _FileInfo.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
 
-namespace ConsoleApp3
+
+namespace SearchFiles
 {
     internal class _FileInfo
     {
