f5c46ee1d3ebebceea3b11b8d05a04a6e3dfe00b
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-03-29 08:09:16 +0300

    Переработал условие, вынес в цикл

--- Program.cs
+++ Program.cs
@@ -7,17 +7,19 @@ namespace SearchFiles
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter dirrectory for search: "); // Добавить бесконечный цикл для ввода директории, как только ввел выходим
+            Console.Write("Enter dirrectory for search: ");
             string _MyDirecTory = Console.ReadLine();
-            if (_MyDirecTory == string.Empty)
+
+            while (_MyDirecTory == string.Empty)
             {
-                Console.WriteLine("Enter dirrectory for search:  ");
-                _MyDirecTory= Console.ReadLine();
+                Console.Write("Enter dirrectory for search:  ");
+                _MyDirecTory = Console.ReadLine();
             }
 
             Console.Write("Enter extension for search: ");
             string _Extension = Console.ReadLine();
-            if (_Extension == string.Empty)
+
+            while (_Extension == string.Empty)
             {
                 Console.Write("Enter extension for search: ");
                 _Extension = Console.ReadLine();
